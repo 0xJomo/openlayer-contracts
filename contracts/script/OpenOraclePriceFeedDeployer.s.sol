18065cbcb3d071f5dbc845bdce7dab4346dcd4a4
kaitaowu <kaitaowu@umich.edu>
2024-05-09 14:00:18 -0700

    Rename priceFeed deployer

--- contracts/script/OpenOraclePriceFeedDeployer.s.sol
+++ contracts/script/OpenOraclePriceFeedDeployer.s.sol
@@ -13,9 +13,7 @@ import "forge-std/console.sol";
 
 // # To deploy and verify our contract
 // forge script script/OpenOracleDeployer.s.sol:OpenOracleDeployer --rpc-url http://127.0.0.1:8545  --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --broadcast -vvvv
-contract OpenOracleSepoliaPriceFeedDeployer is Script, Utils {
-    string public deployConfigPath = string(bytes("./script/config/sepolia/testnet.config.json"));
-
+contract OpenOraclePriceFeedDeployer is Script, Utils {
     uint8 taskTypeLower = 5;
     uint8 taskTypeUpper = 13;
     uint8 responderThreshold = 3;
